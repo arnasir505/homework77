20327e5f77d51b3e0ed88798ba1cf1155b5b8ad3
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-03-29 17:44:10 +0600

    Create container for messages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Appbar from './components/Appbar/Appbar';
 import MessageForm from './components/MessageForm/MessageForm';
+import Messages from './components/Messages/Messages';
 
 function App() {
   return (
     <>
       <Appbar />
       <MessageForm />
+      <Messages />
     </>
   );
 }
---
+++ frontend/src/components/Messages/Messages.tsx
@@ -0,0 +1,8 @@
+import { Container } from '@mui/material';
+import React from 'react';
+
+const Messages: React.FC = () => {
+  return <Container sx={{ py: 3 }}>Messages</Container>;
+};
+
+export default Messages;
